1f366bda1a7a112ff1cc167c4238ad772294f4c8
spetrov1 <svetoslav.petrov@lights.digital>
2021-01-30 12:49:47 +0200

    Teacher waiting room design more consistent with other pages now. Queue is displayed correctly. Needs to send individual request to server, now I reuse code.

--- script/waiting-room-view.js
+++ script/waiting-room-view.js
@@ -1,59 +1,129 @@
-const urlParams = new URLSearchParams(window.location.search);
-const roomId = urlParams.get('roomId');
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const roomId = urlParams.get('roomId')
 
-// TODO try to send only one variable, not object
-const data = {
-    roomId
-};
+function updatePageDetails(){
 
-const settings = {
-    method: 'POST',
-    headers: {
-        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    },
-    body: `data=${JSON.stringify(data)}`
-};
+    var url = 'src/api.php/get-room-details';
+    var room = {
+        roomId
+    }
 
-updateWaitingRoomStudents();
-
-
-
-
-function updateWaitingRoomStudents() {
-    var url = 'src/api.php/get-students-by-waiting-room';
-    ajax(url, settings, updateWaitingStudentsView);
+    var settings = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: `data=${JSON.stringify(room)}`
+    };
+    var res = fetch(url, settings)
+    .then(response => response.json())
+    .then(response => loadDeatails(response))
+    .catch(error => console.log(error));
 }
 
-function ajax(url, settings, callback) {
-    fetch(url, settings)
-        .then(response => response.json())
-        .then(response => callback(response))
-        .catch(error => console.log(error));
-}
+updatePageDetails()
+setInterval(function() {
+    updatePageDetails();
+ }, 10000);
 
-function updateWaitingStudentsView(queryResponse) {
-    if (queryResponse["success"]) {
+function loadDeatails(response){
+    if (response['success']) {
+        loadRoomDetails(response['data']["room"]);
+        // loadWaitingTimeLeft(response['data']["currentUser"]);
+        loadQueue(response['data']['queue']);
 
-        var students = queryResponse["data"];
-
-        for (student of students) {
-            let studentName = student["firstName"] + " " + student["lastName"];
-            displayStudent(studentName);
-        }
     } else {
-        window.location = "teacher-home-page.html";
-        alert(queryResponse["error"]);
-        // TODO back to the previous page OR pop up the page that says You dont have access to this resource...
+        window.location = "student-home-page.html";
+
     }
 }
 
-function displayStudent(studentName) {
-    var node = document.createElement("p");
-    node.innerText = studentName;
+function loadRoomDetails(room){
+    var roomName = room['title'];
+    var subject = room['subject'];
+    var message = room['message'];
+    if (!message){
+        message = "Няма съобщение за показване";
+    }
+    document.getElementById('room_name').innerHTML= "Стая: " + roomName;
+    document.getElementById('room_subject').innerHTML= "Предмет: " + subject;
+    document.getElementById('message').innerHTML= message;
+}
 
-    var studentDisplayElement = document.createElement("div");
-    studentDisplayElement.classList.add("waiting-student");
-    studentDisplayElement.appendChild(node);
+// function loadWaitingTimeLeft($user){
+//     var meetTime = new Date($user['meetTime'])
+//     var currentTime = new Date ()
+//     var leftTime =convertSecondsToHourandMinutes((meetTime - currentTime));
+//     document.getElementById('timer').innerHTML= leftTime;
 
-    document.getElementById("info-side-bar").appendChild(studentDisplayElement);
+// }
+
+function convertSecondsToHourandMinutes(miliseconds){
+    var seconds = parseInt(miliseconds/1000, 10); 
+    var sign = '';
+    if (seconds<0){
+        seconds = seconds*-1;
+        sign = '-';
+    }  
+
+    var days = Math.floor(seconds / (3600*24));
+    if (days>3){
+        return (days + "дни");
+    }
+    var hrs  = Math.floor(seconds / 3600);
+    seconds  -= hrs*3600;
+    var mnts = Math.floor(seconds / 60);
+    seconds  -= mnts*60;
+    return(sign + hrs+":" + mnts.toString().padStart(2, "0"));
+}
+
+
+function loadQueue(queue){
+    clearQueue();
+    if (queue.length > 5){
+        queue = queue.slice(0, 5)
+    }
+
+    for (queueElement of queue){
+        createElement(queueElement, queue.indexOf(queueElement));
+    }
+
+}
+function clearQueue(){
+    var queue = document.getElementById("queue");
+    queue.innerHTML = "";
+    var title = document.createElement("p");
+    title.classList.add("section-title");
+    title.innerHTML = 'Опашка от чакащи';
+    queue.appendChild(title)
+}
+
+function createElement(elem, index){
+    var userName = elem['firstName'] + ' ' + elem['lastName'];
+    var queueIndex = index + 1;
+    var userImageUrl = "storage/"+ elem['Image'];
+
+
+    var queueElem = document.createElement("div");
+    queueElem.classList.add("queue-element");
+
+    var image = document.createElement("img");
+    image.classList.add("user-image");
+    image.src = userImageUrl;
+    queueElem.appendChild(image);
+
+    
+    var name = document.createElement("p");
+    name.classList.add("name");
+    name.innerText =  userName;
+    queueElem.appendChild(name);
+
+
+    var number = document.createElement("p");
+    number.classList.add("number");
+    number.innerText = queueIndex;
+    queueElem.appendChild(number);
+
+    document.getElementById("queue").appendChild(queueElem);
 }
